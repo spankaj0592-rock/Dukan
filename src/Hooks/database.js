0ef87bc589d17267d11d3ff7e029b6172b55a2ea
spankajchauhan <96768418+spankajchauhan@users.noreply.github.com>
2022-08-31 11:10:31 +0530

    add

--- src/Hooks/database.js
+++ src/Hooks/database.js
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 
 const Database = () => {
   const [Data, setData] = useState([]);
-  const[DataCatogories, setDataCat] = useState([]);
-console.log("sds"+DataCatogories);
+  // const[DataCatogories, setDataCat] = useState([]);
+console.log(Data);
 
 
 useEffect(() => {
@@ -14,8 +14,8 @@ fetch('http://localhost:3000/db')
   .then(res => {
     return res.json()
     .then(data =>{
-      setData(data.sliderItems)
-      setDataCat(data.categories);
+      setData(data)
+      
      
       
     })
